c582ad67869d42f52dfb34a37a458a5c146aba47
corina_todea <corina.todea@oracle.com>
2021-11-01 10:00:36 +0200

    Cloud Foundation

---
+++ cloud-foundation/modules/cloud-foundation/dns/variables.tf
@@ -0,0 +1,80 @@
+variable "compartments" {
+  type = map(string)
+}
+
+variable "instances" {
+  type = map(any)
+}
+
+variable "load_balancer_params" {
+  type = map(object({
+    lb_id             = string
+    comp_id           = string
+  }))
+}
+variable "zone_params" {
+  description = "The parameters of the zones: zone_name, zone_type, compartment_id"
+  type        = map(object({
+    compartment_name  = string
+    zone_name         = string
+    zone_type         = string
+    external_masters  = list(object({
+      ip    = string
+    }))
+  }))
+}
+
+variable dns_records_params {
+  description = "The DNS records for the domains(zones)"
+  type = map(object({
+    zone_name        = string
+    domain           = string
+    rtype            = string
+
+    dns_items = list(object({
+      domain   = string
+      rdata    = string
+      rtype    = string
+      ttl      = number
+      use_lb        = bool
+      use_instance  = bool
+      lb_name       = string
+      instance_name = string
+    }))
+  }))
+}
+
+variable dns_resolver_rules { 
+    description = "DNS Resolver rules" 
+    type = map(object({ 
+        resolver_id =string, #fromdatasource 
+        scope = string, # always PRIVATE 
+        rules = list(object({ 
+            action = string, 
+            destination_address = string, 
+            source_endpoint_name = string, 
+            client_address_conditions = string, 
+            #qname_cover_conditions = string, 
+        })) 
+    })) 
+    default = {}
+}
+
+
+variable "dns_resolver_endpoints" { 
+    description = "DNS resolvers endpoints" 
+    type = map(object({ 
+        is_forwarding = bool, 
+        is_listening = bool, 
+        resolver_id = string, 
+        subnet_id = string, 
+        scope = string, # always PRIVATE 
+        endpoint_type = string, 
+        forwarding_address = string, 
+        listening_address = string, 
+        nsg_ids = string, 
+    })) 
+    default = {}
+}
+
+
