dd41d0cd47639df6088c8e9efac615a2fbe99ba8
ionel_panaitescu <ionel.panaitescu@oracle.com>
2022-01-10 16:25:36 +0200

    added the Machine-learning-platform-on-Autonomous-Data-Warehouse solution + golden-gate module

--- cloud-foundation/modules/cloud-foundation-library/golden-gate/main.tf
+++ cloud-foundation/modules/cloud-foundation-library/golden-gate/main.tf
@@ -1,22 +1,16 @@
-## Copyright © 2021, Oracle and/or its affiliates. 
+## Copyright © 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 resource "oci_golden_gate_deployment" "this" {
   for_each                = var.deployment_params
-  compartment_id          = var.compartment_ids[each.value.compartment]
+  compartment_id          = each.value.compartment_id
   cpu_core_count          = each.value.cpu_core_count
   deployment_type         = each.value.deployment_type
-  subnet_id               = var.subnet_ids[each.value.subnet_id]
+  subnet_id               = each.value.subnet_id
   license_model           = each.value.license_model
   display_name            = each.value.display_name
   is_auto_scaling_enabled = each.value.is_auto_scaling_enabled
-  // fqdn = each.value.fqdn
-  // freeform_tags = each.value.freeform_tags
-  // is_public = each.value.is_public
-  // nsg_ids = each.value.nsg_ids
-  // defined_tags = each.value.defined_tags
-  // deployment_backup_id = each.value.deployment_backup_id
-  // description = each.value.description
+  defined_tags            = each.value.defined_tags
   dynamic "ogg_data" {
     iterator = ogg_data
     for_each = each.value.ogg_data
@@ -26,14 +20,4 @@ resource "oci_golden_gate_deployment" "this" {
       deployment_name = ogg_data.value.deployment_name
     }
   }
-
-  //  dynamic "timeouts" {
-  //    for_each = var.timeouts
-  //    content {
-  //      create = timeouts.value.create
-  //      delete = timeouts.value.delete
-  //      update = timeouts.value.update
-  //    }
-  //  }
-
 }
--- cloud-foundation/modules/cloud-foundation-library/golden-gate/variables.tf
+++ cloud-foundation/modules/cloud-foundation-library/golden-gate/variables.tf
@@ -1,41 +1,21 @@
-## Copyright © 2021, Oracle and/or its affiliates. 
+## Copyright © 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-variable "compartment_ids" {
-  type = map(string)
-}
-
-variable "subnet_ids" {
-  type = map(string)
-}
-
-
 variable "deployment_params" {
   type = map(object({
-    compartment             = string
+    compartment_id          = string
     cpu_core_count          = number
     deployment_type         = string
     subnet_id               = string
     license_model           = string
     display_name            = string
     is_auto_scaling_enabled = bool
+    defined_tags            = map(string)
     ogg_data = set(object(
       {
         admin_password  = string
         admin_username  = string
         deployment_name = string
-        #optional params
-        //      certificate     = string
-        //      key             = string
       }
     ))
-    #Optional paramenters
-    //  fqdn = string
-    //  freeform_tags = map(string)
-    //  is_public = bool
-    //  nsg_ids = set(string)
-    //  defined_tags = map(string)
-    //  deployment_backup_id = string
-    //  description = string
   }))
 }
