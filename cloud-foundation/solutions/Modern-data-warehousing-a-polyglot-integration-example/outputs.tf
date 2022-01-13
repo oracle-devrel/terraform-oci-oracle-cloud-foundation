78ee5ef9d3e01033d36ea91dc215669142cf9cfe
ionel_panaitescu <ionel.panaitescu@oracle.com>
2022-01-13 15:03:33 +0200

    added Modern-data-warehousing-a-polyglot-integration-example solution

---
+++ cloud-foundation/solutions/Modern-data-warehousing-a-polyglot-integration-example/outputs.tf
@@ -0,0 +1,50 @@
+# Copyright © 2022, Oracle and/or its affiliates.
+# All rights reserved. Licensed under the Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+output "ADW_Service_Console_URL" {
+  value = module.adw.ADW_Service_Console_URL
+}
+
+output "Analytics_URL" {
+  value = module.oac.Analytics_URL
+}
+
+output "Instructions" {
+  value = "Please use the ADW URL to login by using the user admin and the password that you have provided."
+}
+
+output "Buckets" {
+  value = module.os.buckets
+}
+
+output "DataCatalog" {
+  value = module.datacatalog.datacatalog
+}
+
+output "Odi" {
+  value = module.odi.odi
+}
+
+output "Datascience" {
+  value = module.datascience.datascience
+}
+
+output "Notebook" {
+  value = module.datascience.notebook
+}
+
+output "deployment_id" {
+  value = module.golden_gate_deployment.deployment_id
+}
+
+output "private_ip" {
+  value = module.golden_gate_deployment.deployment_ip
+}
+
+output "deployment_url" {
+  value = module.golden_gate_deployment.deployment_url
+}
+
+output "golgen_gate_console" {
+  value = "The GoldenGate console is only accessible through a private URL from a Compute instance using the same VCN as the GoldenGate deployment, or through a bastion host."
+}
