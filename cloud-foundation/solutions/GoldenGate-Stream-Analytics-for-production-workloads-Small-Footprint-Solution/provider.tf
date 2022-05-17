17c0ccd2465a83801a70f807ece51f21437fbaf5
ionelpanaitescu <ionel.panaitescu@oracle.com>
2022-05-17 17:46:01 +0300

    fixes

--- cloud-foundation/solutions/GoldenGate-Stream-Analytics-for-production-workloads-Small-Footprint-Solution/provider.tf
+++ cloud-foundation/solutions/GoldenGate-Stream-Analytics-for-production-workloads-Small-Footprint-Solution/provider.tf
@@ -15,7 +15,7 @@ provider "oci" {
   tenancy_ocid         = var.tenancy_ocid
   user_ocid            = var.user_ocid
   fingerprint          = var.fingerprint
-  private_key          = var.private_key
+  private_key_path     = var.private_key_path
   region               = var.region
   disable_auto_retries = false
 }
@@ -25,7 +25,7 @@ provider "oci" {
   tenancy_ocid         = var.tenancy_ocid
   user_ocid            = var.user_ocid
   fingerprint          = var.fingerprint
-  private_key          = var.private_key
+  private_key_path     = var.private_key_path
   region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = false
 }
