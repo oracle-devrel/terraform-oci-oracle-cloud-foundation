1704d5303a8d2c88399fe9c1d159c9d07c207eb7
ïonel panaitescu <onel.panaitescu@oracle.com>
2022-05-17 17:20:30 +0300

    fixes on keygen and ggsa small solution provider

--- cloud-foundation/modules/cloud-foundation-library/keygen/outputs.tf
+++ cloud-foundation/modules/cloud-foundation-library/keygen/outputs.tf
@@ -18,14 +18,14 @@ output "CertPem" {
 }
 
 resource local_file "cert_demo" {
-  sensitive_content = tls_self_signed_cert.demo_cert.cert_pem
+  content              = tls_self_signed_cert.demo_cert.cert_pem
   filename             = "cert_demo.pem"
   file_permission      = 0777
   directory_permission = 0777
 }
 
 resource local_file "cert_private_key" {
-  sensitive_content = tls_private_key.ss_private_key.private_key_pem
+  content              = tls_private_key.ss_private_key.private_key_pem
   filename             = "cert_private_key.pem"
   file_permission      = 0777
   directory_permission = 0777
