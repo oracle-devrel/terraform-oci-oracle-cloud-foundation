5fd7b74f0eece0ac903c5c65ff08a460ff144456
JBAnderson5 <jb.anderson@oracle.com>
2022-09-28 14:05:55 -0700

    added policy header to all code files in my modules

--- cloud-foundation/modules/cloud-foundation-library/identity/module/application.tf
+++ cloud-foundation/modules/cloud-foundation-library/identity/module/application.tf
@@ -1,3 +1,6 @@
+# Copyright © 2022, Oracle and/or its affiliates.
+# All rights reserved. Licensed under the Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 
 # input
 
