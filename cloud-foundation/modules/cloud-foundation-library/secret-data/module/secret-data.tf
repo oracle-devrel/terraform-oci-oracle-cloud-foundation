594a6c62c607c5a230d7f04ea4165cd6b7f25bac
JB Anderson <jb.anderson@oracle.com>
2023-01-26 21:14:48 -0800

    added lookup by secret name support for secret-data

--- cloud-foundation/modules/cloud-foundation-library/secret-data/module/secret-data.tf
+++ cloud-foundation/modules/cloud-foundation-library/secret-data/module/secret-data.tf
@@ -4,12 +4,6 @@
 
 # inputs
 
-/*
-variable "load_from_secrets" {
-    type = bool
-    default = true
-}
-*/
 
 variable "secret_ocids"{
     type = map(string)
@@ -17,29 +11,26 @@ variable "secret_ocids"{
     description = "a map of strings where key is used for output reference. the ocid inside should refer to a secret's ocid"
 }
 
-
-
-
-#gets a secret bundle which is how you read from a secret
-data "oci_secrets_secretbundle" "these" {
-    for_each = var.secret_ocids
-
-    secret_id = each.value
+variable "secret_names" {
+    type = map(string)
+    default = {}
+    description = "an object map where key is used for output reference. The value pair should refer to a secret's full name. requires a compartment input as well"
 }
 
-# used to get vault and key id - also in oci_vault_secrets datasource above, but not guaranteed to create that
-data "oci_vault_secret" "this" {
-    secret_id = data.oci_secrets_secretbundle.these[keys(var.secret_ocids)[0]].secret_id
+variable "compartment" {
+    type = string 
+    default = null 
+    decription = "required for looking up secrets by name."
 }
 
 
 # outputs
 
 output "contents" {
-    value = {for key, value in var.secret_ocids :
+    value = {for key, value in local.secret_ocids :
         key => jsondecode(base64decode(data.oci_secrets_secretbundle.these[key].secret_bundle_content[0].content))
     }
-    description = "secret contents decoded from base 64, and then from json. returned in a simple key-value map where key is the same name as your input map"
+    description = "secret contents decoded from base 64, and then from json. returned in a simple key-value map where key is the same name as your input maps"
 }
 
 output "vault" {
@@ -52,9 +43,39 @@ output "key" {
     description = "the ocid of the key that encrypted this secret"
 }
 
-# TODO: should we also return the compartment the secret was created in?
 
 # logic
+
+
+# look up secrets by name to get their ocids
+data "oci_vault_secrets" "these" {
+    for_each = var.secret_names
+            
+    compartment_id = var.compartment
+
+    name = each.value
+}
+
+# add secret names list to secret ocids
+locals {
+
+    secret_ocids = merge (var.secret_ocids, 
+        {for key,value in var.secret_names: key => data.oci_vault_secrets.these[key].secret_id}
+    )
+  
+}
+
+#gets a secret bundle which is how you read from a secret
+data "oci_secrets_secretbundle" "these" {
+    for_each = local.secret_ocids
+
+    secret_id = each.value
+}
+
+# used to get vault and key id - also in oci_vault_secrets datasource above, but not guaranteed to create that
+data "oci_vault_secret" "this" {
+    secret_id = data.oci_secrets_secretbundle.these[keys(local.secret_ocids)[0]].secret_id
+}
 
 
 # resource or mixed module blocks
