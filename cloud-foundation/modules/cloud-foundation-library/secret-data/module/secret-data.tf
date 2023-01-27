415d0cecd01d1016179ea8dfc1cbf59afcbf58a2
JB Anderson <jb.anderson@oracle.com>
2023-01-27 10:12:09 -0800

    fixing secret mapping lookup error

--- cloud-foundation/modules/cloud-foundation-library/secret-data/module/secret-data.tf
+++ cloud-foundation/modules/cloud-foundation-library/secret-data/module/secret-data.tf
@@ -60,8 +60,8 @@ data "oci_vault_secrets" "these" {
 locals {
 
     secret_ocids = merge (var.secret_ocids, 
-        {for key,value in data.oci_vault_secrets.these : key => value.id}
-        # var.secret_names: key => data.oci_vault_secrets.these[key].secrets[0].id}
+        {for key,value in var.secret_names: key => data.oci_vault_secrets.these[key].secrets[0].id}
+        #data.oci_vault_secrets.these : key => value.id}
     )
   
 }
